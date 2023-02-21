960ed6a7beb11c3cda18ed2bfba9c52b185478bf
cbarriuso <carlosbarriuso@yahoo.es>
2023-02-21 17:58:14 +0100

    Componente buscar por pais

--- src/app/pais/pais.module.ts
+++ src/app/pais/pais.module.ts
@@ -5,6 +5,7 @@ import { PorPaisComponent } from './pages/por-pais/por-pais.component';
 import { PorRegionComponent } from './pages/por-region/por-region.component';
 
 import { VerPaisComponent } from './pages/ver-pais/ver-pais.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { VerPaisComponent } from './pages/ver-pais/ver-pais.component';
     VerPaisComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   exports: [
     PorCapitalComponent,
