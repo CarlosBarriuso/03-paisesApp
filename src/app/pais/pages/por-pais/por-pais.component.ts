9b057b7f6d2d2f086730d7b6a90f460e9b9fae68
cbarriuso <carlosbarriuso@yahoo.es>
2023-02-21 18:58:22 +0100

    manejo de errores

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -8,17 +8,23 @@ import { PaisService } from '../../services/pais.service';
 })
 export class PorPaisComponent {
 
-  termino = 'Holamundo';
+  termino: string = 'Holamundo';
+  hayError: boolean = false;
 
   // Inyectamos el servicio para poder usar el servicio
   constructor( private paisService: PaisService) { }
 
   buscar() {
+    this.hayError = false;
     console.log(this.termino);
     //para que un Observable se dispare hace falta tener un Suscribe
     this.paisService.buscarPais( this.termino )
+    //como 2º valor subcribe puede recibir el error y lo vamos a rescatar
     .subscribe( respuesta => {
+      
       console.log(respuesta);
+    }, (err) => {
+      this.hayError = true;
     });
 
   }  
